ea0473018aa8f88016791e4eafa15b06c569ed37
ju-nong <iiiuooyoo@gmail.com>
2022-07-21 17:54:37 +0900

    edit Access Permission

--- src/App.vue
+++ src/App.vue
@@ -16,49 +16,13 @@ import { useRouteStore } from "@store/router";
 import { useUserStore } from "@store/user";
 import { useRouter } from "vue-router";
 
-function chkPer(type, domain) {
-    console.log(type, domain);
-    if (type == "비") {
-        return !(domain == "Second" || domain == "Third");
-    }
-
-    if (type == "A" || type == "B") {
-        return !(domain == "Third" || domain == "Fourth");
-    }
-
-    return !(domain == "Second" || domain == "Fourth");
-}
-
 export default {
     name: "App",
     components: { Header, Tab, Footer },
     setup() {
         const { meta } = useRouteStore();
-        const user = reactive({
-            store: useUserStore(),
-            type: computed(() => user.store.getUser),
-        });
 
-        const router = useRouter();
-
-        router.beforeEach((to, from, next) => {
-            const toName = to.name; // 후 페이지
-            const fromName = from.name; // 전 페이지
-            const type = user.type;
-            const noPer = to.meta.noPer;
-            let path = "/";
-
-            if (fromName != undefined) {
-                if (!noPer.includes(user.type)) {
-                    path = null;
-                }
-            } else {
-                path = null;
-            }
-            next(path);
-        });
-
-        return { meta, user };
+        return { meta };
     },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,30 @@
 import { createWebHistory, createRouter } from "vue-router";
+import { useUserStore } from "@store/user";
 
 import { First, Second, Third, Fourth } from "@routes";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [...First, ...Second, ...Third, ...Fourth],
+});
+
+router.beforeEach((to, from, next) => {
+    const user = useUserStore();
+
+    const fromName = from.name; // 전 페이지
+    const noPer = to.meta.noPer;
+    let path = "/";
+
+    if (fromName != undefined) {
+        if (!noPer.includes(user.getUser)) {
+            path = null;
+        } else {
+            alert("접근 권환이 없습니다.");
+        }
+    } else {
+        path = null;
+    }
+    next(path);
 });
 
 export default router;
